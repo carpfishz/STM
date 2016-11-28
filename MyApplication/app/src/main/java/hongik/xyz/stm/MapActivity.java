69ded3250c25351c14f06e296a82240c09449873
yeansang an <ysan1991@gmail.com>
2016-11-28 14:59:58 +0900

    gradle fix

--- MyApplication/app/src/main/java/hongik/xyz/stm/MapActivity.java
+++ MyApplication/app/src/main/java/hongik/xyz/stm/MapActivity.java
@@ -39,8 +39,8 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker test"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
