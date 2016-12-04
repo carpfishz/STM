d46af2686d6dd97578b0f7fc2821d61526a27308
carpfishz <lsml3541@gmail.com>
2016-12-04 16:46:28 +0900

    upload core.jar, add gradle option and generate barcode-128

--- MyApplication/app/src/main/java/hongik/xyz/stm/MainActivity.java
+++ MyApplication/app/src/main/java/hongik/xyz/stm/MainActivity.java
@@ -1,21 +1,36 @@
 package hongik.xyz.stm;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button pointButton;
     private Button eventButton;
     private Button mapButton;
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img = (ImageView) findViewById(R.id.imageView1);
+        String strBarcode = "987654321";
+        Bitmap barcode = createBarcode(strBarcode);
+        img.setImageBitmap(barcode);
+        img.invalidate();
+
 
         mapButton = (Button) findViewById(R.id.map_button);
 
@@ -27,5 +42,27 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public Bitmap createBarcode(String code){
+
+
+        Bitmap bitmap =null;
+        MultiFormatWriter gen = new MultiFormatWriter();
+        try {
+            final int WIDTH = 840;
+            final int HEIGHT = 160;
+            BitMatrix bytemap = gen.encode(code, BarcodeFormat.CODE_128, WIDTH, HEIGHT);
+            bitmap = Bitmap.createBitmap(WIDTH, HEIGHT, Bitmap.Config.ARGB_8888);
+            for (int i = 0 ; i < WIDTH ; ++i)
+                for (int j = 0 ; j < HEIGHT ; ++j) {
+                    bitmap.setPixel(i, j, bytemap.get(i,j) ? Color.BLACK : Color.WHITE);
+                }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return bitmap;
     }
 }
